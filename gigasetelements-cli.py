1dc55847d30f8820c3c73b9aa2074be8a3f9e2af
dynasticorpheus <dynasticorpheus@gmail.com>
2015-02-25 13:06:12 +0100

    code cleanup, pep8

--- gigasetelements-cli.py
+++ gigasetelements-cli.py
@@ -37,7 +37,7 @@ parser.add_argument('-e', '--events', help='show last <number> of events', type=
 parser.add_argument('-m', '--modus', help='set modus', required=False, choices=('home', 'away', 'custom'))
 parser.add_argument('-s', '--status', help='show system / sensor status', action='store_true', required=False)
 parser.add_argument('-w', '--warning', help='suppress authentication warnings', action='store_true', required=False)
-parser.add_argument('-v', '--version', help='show version', action='version', version="%(prog)s version "+str(_version_))
+parser.add_argument('-v', '--version', help='show version', action='version', version="%(prog)s version " + str(_version_))
 args = parser.parse_args()
 
 
@@ -51,10 +51,10 @@ def configure():
         pass
     else:
         if os.path.exists(args.config) == False:
-            print('[-]  File does not exist '+args.config)
+            print('[-]  File does not exist ' + args.config)
             print
             exit()
-        print('[-]  Reading configuration from '+args.config)
+        print('[-]  Reading configuration from ' + args.config)
         config = ConfigParser.ConfigParser()
         config.read(args.config)
         if args.username is None:
@@ -112,7 +112,7 @@ def connect():
 
 def modus_switch():
     switch = {"intrusion_settings": {"active_mode": args.modus}}
-    r4 = s.post('https://api.gigaset-elements.de/api/v1/me/basestations/'+my_basestation, data=json.dumps(switch))
+    r4 = s.post('https://api.gigaset-elements.de/api/v1/me/basestations/' + my_basestation, data=json.dumps(switch))
     return
 
 
@@ -121,19 +121,19 @@ def pb_message():
         pass
     else:
         pb = PushBullet(args.notify)
-        push = pb.push_note("Gigaset Elements", 'Modus set to '+args.modus.upper())
+        push = pb.push_note("Gigaset Elements", 'Modus set to ' + args.modus.upper())
         print "[-]  PushBullet notification sent"
     return
 
 
 def list_events():
-    print "[-]  Showing last "+str(args.events)+" event(s)"
-    r5 = s.get('https://api.gigaset-elements.de/api/v1/me/events?limit='+str(args.events))
+    print "[-]  Showing last " + str(args.events) + " event(s)"
+    r5 = s.get('https://api.gigaset-elements.de/api/v1/me/events?limit=' + str(args.events))
     event_data = r5.json()
     for item in event_data["events"]:
         try:
             print("[-] "),
-            print(time.strftime('%m/%d/%Y %H:%M:%S',  time.localtime(int(item['ts'])/1000))),
+            print(time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(int(item['ts']) / 1000))),
             print item['type'],
             print item['o']['friendly_name']
         except KeyError:
@@ -170,7 +170,7 @@ def status():
 
     r7 = s.get('https://api.gigaset-elements.de/api/v1/me/events?limit=1')
     status_data = r7.json()
-    print("[-]  Home status "+status_data["home_state"])
+    print("[-]  Home status " + status_data["home_state"])
     return
 
 
@@ -182,29 +182,24 @@ try:
     configure()
     connect()
 
-
     if args.modus is None:
         pass
     else:
         modus_switch()
-        print "[-]  Modus set to "+args.modus.upper()
+        print "[-]  Modus set to " + args.modus.upper()
         pb_message()
-
 
     if args.status is not True:
         pass
     else:
         status()
 
-
     if args.events is None:
         pass
     else:
         list_events()
 
-
     print
 
 except KeyboardInterrupt:
     print("[-]  CTRL+C detected program halted")
-
