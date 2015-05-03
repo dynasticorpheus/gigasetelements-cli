a1d69082f5386c456095836a74f88bf3ec8edd00
dynasticorpheus <dynasticorpheus@gmail.com>
2015-05-03 21:44:53 +0200

    show ssid info when camera connection is wifi

--- gigasetelements/gigasetelements.py
+++ gigasetelements/gigasetelements.py
@@ -315,15 +315,17 @@ def camera():
         for item in camera_data:
             try:
                 print('[-] ') + camera_data[0]['friendly_name'] + ' ' + color(camera_data[0]['status']) + ' | firmware ' + color(camera_data[0]['firmware_status']),
-                print(' | quality ' + color(camera_data[0]['settings']['quality']) + ' | nightmode ' + color(camera_data[0]['settings']['nightmode']) + ' | mic ' + color(camera_data[0]['settings']['mic'])),
-                print(' | connection ' + color(camera_data[0]['settings']['connection']) + ' | motion detection ' + color(camera_data[0]['motion_detection']['status']))
+                print('| quality ' + color(camera_data[0]['settings']['quality']) + ' | nightmode ' + color(camera_data[0]['settings']['nightmode']) + ' | mic ' + color(camera_data[0]['settings']['mic'])),
+                print('| motion detection ' + color(camera_data[0]['motion_detection']['status']) + ' | connection ' + color(camera_data[0]['settings']['connection'])),
+                if camera_data[0]['settings']['connection'] == 'wifi':
+                    print('| ssid ' + color(camera_data[0]['wifi_ssid']))
             except KeyError:
                 print
                 continue
         stream_data = restget(url_camera + '/' + camera_data[0]['id'] + '/liveview/start')
-        log('Stream 1 | m3u8 | ' + stream_data['uri']['m3u8'])
-        log('Stream 2 | rtmp | ' + stream_data['uri']['rtmp'])
-        log('Stream 3 | rtsp | ' + stream_data['uri']['rtsp'])
+        log('Camera stream 1 | m3u8 | ' + stream_data['uri']['m3u8'])
+        log('Camera stream 2 | rtmp | ' + stream_data['uri']['rtmp'])
+        log('Camera stream 3 | rtsp | ' + stream_data['uri']['rtsp'])
     return
 
 
