6f8012346e2080761ce0ab67c1b0143f68cfbacb
dynasticorpheus <dynasticorpheus@gmail.com>
2015-10-07 08:09:55 +0200

    version bump to 1.3.6

--- gigasetelements/gigasetelements.py
+++ gigasetelements/gigasetelements.py
@@ -21,7 +21,7 @@ from pushbullet import PushBullet
 
 
 _AUTHOR_ = 'dynasticorpheus@gmail.com'
-_VERSION_ = '1.3.5'
+_VERSION_ = '1.3.6'
 
 parser = argparse.ArgumentParser(description='Gigaset Elements - Command-line Interface by dynasticorpheus@gmail.com')
 parser.add_argument('-c', '--config', help='fully qualified name of configuration-file', required=False)
