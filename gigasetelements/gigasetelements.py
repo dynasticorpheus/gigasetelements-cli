720967cf3c568294753f1ff957924b0ddb9d2326
dynasticorpheus <dynasticorpheus@gmail.com>
2016-05-08 09:04:46 +0200

    point url to 1.5 release

--- gigasetelements/gigasetelements.py
+++ gigasetelements/gigasetelements.py
@@ -46,7 +46,7 @@ URL_CAMERA = 'https://api.gigaset-elements.de/api/v1/me/cameras'
 URL_HEALTH = 'https://api.gigaset-elements.de/api/v2/me/health'
 URL_CHANNEL = 'https://api.gigaset-elements.de/api/v1/me/notifications/users/channels'
 URL_RELEASE = 'https://pypi.python.org/pypi/gigasetelements-cli/json'
-URL_USAGE = 'https://goo.gl/wjLswA'
+URL_USAGE = 'https://goo.gl/oHJ565'
 
 URL_SWITCH = '/json.htm?type=command&param=switchlight&switchcmd='
 URL_ALERT = '/json.htm?type=command&param=udevice&idx='
